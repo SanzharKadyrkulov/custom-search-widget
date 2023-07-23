3a9ace282ebf590078dce3d1601164e32cadd305
SanzharKadyrkulov <kadyrkulov.980@gmail.com>
2023-07-23 20:07:34 +0600

    comments

--- main.js
+++ main.js
@@ -113,6 +113,7 @@ template.innerHTML = `
 class SearchWidget extends HTMLElement {
 	constructor() {
 		super();
+		//? creating a shadow dom
 		const shadow = this.attachShadow({ mode: "open" });
 		shadow.append(template.content.cloneNode(true));
 
@@ -120,9 +121,16 @@ class SearchWidget extends HTMLElement {
 		this.selectTo = shadow.querySelector("#select-to");
 
 		this.dateFrom = shadow.querySelector("#date-from");
-		this.dateFrom.setAttribute("min", getToday());
 		this.dateTo = shadow.querySelector("#date-to");
 
+		this.checkbox = shadow.querySelector("#checkbox");
+
+		this.button = shadow.querySelector("#search-btn");
+
+		// ? set min value for dateFrom input
+		this.dateFrom.setAttribute("min", getToday());
+
+		//? set min value for dateTo input when date from changes
 		this.dateFrom.addEventListener("change", () => {
 			const from = this.dateFrom.value;
 			const to = this.dateTo.value;
@@ -133,26 +141,29 @@ class SearchWidget extends HTMLElement {
 			}
 		});
 
-		this.checkbox = shadow.querySelector("#checkbox");
+		//? making dateTo input disabled, when checkbox is checked
 		this.checkbox.addEventListener("change", (e) => {
 			this.dateTo.disabled = e.target.checked;
 		});
 
-		this.button = shadow.querySelector("#search-btn");
+		//? eventListener for form submit
 		this.button.addEventListener("click", (e) => {
 			e.preventDefault();
 
+			//? checking for empty dateFrom input
 			if (!this.dateFrom.value) {
 				showRedOutline(this.dateFrom);
 				showToast("Please select date from");
 				return;
 			}
+			//? checking for empty dateTo input
 			if (!this.dateTo.value && !this.checkbox.checked) {
 				showRedOutline(this.dateTo);
 				showToast("Please select date to");
 				return;
 			}
 
+			//? create new CustomEvent with data from form
 			const event = new CustomEvent("search", {
 				detail: {
 					startPoint: this.selectFrom.value,
@@ -167,6 +178,7 @@ class SearchWidget extends HTMLElement {
 		this.cities = [];
 	}
 
+	//? function to add options to select tags
 	renderCities() {
 		let options = "";
 		this.cities.forEach(({ country, city }) => {
@@ -183,23 +195,28 @@ class SearchWidget extends HTMLElement {
 
 	set cities(arr) {
 		this.data = arr;
+		//? adding options
 		this.renderCities();
 	}
 }
 
+// ? defining new html tag
 customElements.define("search-widget", SearchWidget);
 
+//? getting elements from html
 const search = document.querySelector("#first");
 const search2 = document.querySelector("#second");
 const toast = document.querySelector("#toast");
 const modal = document.querySelector(".modal");
 const closeModal = document.querySelector(".close");
 
+//? setting cities to search widgets
 getCities().then(({ data }) => {
 	search.cities = data;
-	search2.cities = [{ city: "bishkek", country: "kyrgyzstan" }];
 });
+search2.cities = [{ city: "bishkek", country: "kyrgyzstan" }];
 
+//? eventListener to catch custom 'search' event
 search.addEventListener("search", (e) => {
 	showModal();
 	console.log(e, "first widget event");
@@ -209,10 +226,11 @@ search2.addEventListener("search", (e) => {
 	console.log(e, "second widget event");
 });
 
+//? eventListener to close modal
 closeModal.addEventListener("click", hideModal);
 
+//? function to get cities from free API
 async function getCities() {
-	// const res = await fetch("https://countriesnow.space/api/v0.1/countries");
 	const res = await fetch(
 		"https://countriesnow.space/api/v0.1/countries/population/cities"
 	);
@@ -220,6 +238,7 @@ async function getCities() {
 	return data;
 }
 
+//? function to get today's day in YYYY-MM-DD format
 function getToday() {
 	const date = new Date();
 	const year = date.getFullYear();
@@ -229,6 +248,7 @@ function getToday() {
 	return `${year}-${month}-${day}`;
 }
 
+//? function to show toast
 function showToast(message = "Something went wrong") {
 	toast.textContent = message;
 	toast.style.visibility = "visible";
@@ -240,6 +260,7 @@ function showToast(message = "Something went wrong") {
 	}, 2000);
 }
 
+//? outline input when error occurs
 function showRedOutline(elem) {
 	elem.style.outlineColor = "#dc3545";
 	setTimeout(() => {
@@ -248,6 +269,7 @@ function showRedOutline(elem) {
 	elem.focus();
 }
 
+// ? Modal actions
 function showModal() {
 	modal.style.display = "block";
 }
